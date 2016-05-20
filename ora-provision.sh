888638d3f85951c2ddb38cbe303e38b4ac5bc097
venkat shanmugam <vshanmugam@provenir.com>
2016-05-20 13:05:56 -0400

    oracle vm

--- ora-provision.sh
+++ ora-provision.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-#sudo apt-get update -y
+sudo apt-get update -y
 
-#sudo apt-get install alien libaio1 unixodbc vim -y
-#sudo apt-get install unzip dos2unix -y
-cp /pcinstalls/oracle-xe-11.2.0-1.0.x86_64.rpm.zip /tmp
+sudo apt-get install alien libaio1 unixodbc vim -y
+sudo apt-get install unzip dos2unix -y
 
+sudo rm -rf /dev/shm
+sudo mkdir /dev/shm
+sudo mount -t tmpfs shmfs -o size=1500m /dev/shm
+
+#cp /pcinstalls/oracle-xe-11.2.0-1.0.x86_64.rpm.zip /tmp
+cp /pcinstalls/oracle-xe_11.2.0-2_amd64.deb /tmp
 
 #sudo /etc/init.d/oracle-xe stop
 #sudo ps -ef | grep oracle | grep -v grep | awk '{print $2}' | xargs kill
@@ -18,14 +23,16 @@ cp /pcinstalls/oracle-xe-11.2.0-1.0.x86_64.rpm.zip /tmp
 
 cd /tmp
 
-unzip oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+#unzip oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 
-rm oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+#rm oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 
 cd Disk1
 
 echo "Converting rpm to deb ... may take a while"
-sudo alien --scripts -d oracle-xe-11.2.0-1.0.x86_64.rpm
+#sudo alien --scripts -d oracle-xe-11.2.0-1.0.x86_64.rpm
+
+
 
 dos2unix /pcinstalls/checkconfig.txt
 
@@ -37,6 +44,7 @@ dos2unix /pcinstalls/60-oracle.conf
 sudo cp /pcinstalls/60-oracle.conf /etc/sysctl.d/60-oracle.conf
 
 sudo service procps start
+#to check grep -n ".*" /proc/sys/kernel/shmm*
 
 sudo ln -s /usr/bin/awk /bin/awk
 
@@ -48,11 +56,22 @@ sudo dpkg --install oracle-xe_11.2.0-2_amd64.deb
 
 dos2unix /pcinstalls/xe.rsp
 
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+export ORACLE_SID=XE
+export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh`
+export ORACLE_BASE=/u01/app/oracle
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$ORACLE_HOME/bin:$PATH
+
+sudo cat /pcinstalls/bashrc >> ~/.bashrc
+
+exit 0
+
 sudo /etc/init.d/oracle-xe configure responseFile=/pcinstalls/xe.rsp
 
 dos2unix /pcinstalls/bashrc
 
-sudo cat /pcinstalls/bashrc >> ~/.bashrc
+
 
 
 sudo service oracle-xe start
