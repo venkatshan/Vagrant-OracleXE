91b54c8f722cc94a3124158e982c10f02a969df0
venkat shanmugam <vshanmugam@provenir.com>
2016-05-24 15:11:03 -0400

    shm changes

--- installs/startoracle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -rf /dev/shm && mkdir /dev/shm && mount -t tmpfs shmfs -o size=1500m /dev/shm && service oracle-xe start
--- ora-provision.sh
+++ ora-provision.sh
@@ -7,13 +7,15 @@ sudo apt-get install unzip dos2unix -y
 
 sudo rm -rf /dev/shm
 sudo mkdir /dev/shm
-sudo mount -t tmpfs shmfs -o size=1500m /dev/shm
-sudo cp /pcinstalls/startoracle.sh /etc/
-sudo chmod +x /etc/startoracle.sh
+sudo mount -t tmpfs shmfs -o size=2048m /dev/shm
+sudo mkdir /var/lock/subsys
+touch /var/lock/subsys/listener
 
-command="/etc/startoracle.sh"
-job="@Reboot $command"
-sudo cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
+# Mount tmpfs on reboot using RC2
+sudo cp /pcinstalls/S01shm_load /etc/rc2.d/
+sudo dos2unix  /etc/rc2.d/S01shm_load
+sudo chmod 755 /etc/rc2.d/S01shm_load
+
 
 # -- uninstall oracle-xe
 #sudo /etc/init.d/oracle-xe stop
@@ -27,12 +29,12 @@ sudo cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
 
 
 cd /tmp
-wget https://www.dropbox.com/s/6zu6wj26du720ef/oracle-xe_11.2.0-2_amd64.deb?raw=1 -O oracle-xe_11.2.0-2_amd64.deb
+#wget https://www.dropbox.com/s/6zu6wj26du720ef/oracle-xe_11.2.0-2_amd64.deb?raw=1 -O oracle-xe_11.2.0-2_amd64.deb
 
 #---- RPM to DEB
 #-- [pcinstalls] - windows share
 #cp /pcinstalls/oracle-xe-11.2.0-1.0.x86_64.rpm.zip /tmp
-#cp /pcinstalls/oracle-xe_11.2.0-2_amd64.deb /tmp
+cp /pcinstalls/oracle-xe_11.2.0-2_amd64.deb /tmp
 
 #unzip oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 #rm oracle-xe-11.2.0-1.0.x86_64.rpm.zip
@@ -49,17 +51,16 @@ sudo cp /pcinstalls/checkconfig.txt /sbin/chkconfig
 
 sudo chmod 755 /sbin/chkconfig
 
-dos2unix /pcinstalls/60-oracle.conf
+
 sudo cp /pcinstalls/60-oracle.conf /etc/sysctl.d/60-oracle.conf
+dos2unix /etc/sysctl.d/60-oracle.conf
 
 sudo service procps start
 #to check grep -n ".*" /proc/sys/kernel/shmm*
 
 sudo ln -s /usr/bin/awk /bin/awk
 
-sudo mkdir /var/lock/subsys
 
-touch /var/lock/subsys/listener
 
 sudo dpkg --install oracle-xe_11.2.0-2_amd64.deb
 
@@ -73,7 +74,7 @@ export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
 export PATH=$ORACLE_HOME/bin:$PATH
 
 dos2unix /pcinstalls/bashrc
-sudo cat /pcinstalls/bashrc >> ~/.bashrc
+sudo cat /pcinstalls/bashrc >> ~vagrant/.bashrc
 
 dos2unix /pcinstalls/xe.rsp
 sudo /etc/init.d/oracle-xe configure responseFile=/pcinstalls/xe.rsp
